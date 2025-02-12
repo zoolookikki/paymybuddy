e98d92e25559f037e1a5ea9fc06db2872b48bdc2
Laurent Cordier <laurent.cordier28@orange.fr>
2025-02-12 10:14:05 +0100

    initial commit

---
+++ src/main/java/com/cordierlaurent/paymybuddy/PaymybuddyApplication.java
@@ -0,0 +1,13 @@
+package com.cordierlaurent.paymybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymybuddyApplication.class, args);
+	}
+
+}
